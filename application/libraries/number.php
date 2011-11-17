8cb99c0941421119e53a275f59b3b897bd31d846
Dwi Wijonarko <dwijonarko@mint>
2011-11-17 11:28:38 +0700

    membuat library nomor untuk generate nomor surat

---
+++ application/libraries/number.php
@@ -0,0 +1,14 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class Number{
+
+	function generate_number($format=array()){
+		$noprefix=$format['num'];
+		$divisi=$format['divisi'];
+		$perihal=$format['perihal'];
+		$no_last = $noprefix+1;
+		$num = str_pad($no_last,4,'0',STR_PAD_LEFT);
+
+		return $num."/POLTEKOM/".$divisi."/".$perihal."/".date("m.Y");
+	}
+}
+
